4fafcd964f7d1449799245ec455c81fab81fff78
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-11-09 20:28:57 +0600

    first commit

---
+++ src/main/java/com/hawkeye/model/Employee.java
@@ -0,0 +1,15 @@
+package com.hawkeye.model;
+
+import java.sql.Timestamp;
+
+public class Employee {
+	private String empId;
+	private String name;
+	private String email;
+	private String password;
+	private Timestamp creationTime;
+	private Timestamp lastAccess;
+	
+	
+
+}
---
+++ src/main/java/com/hawkeye/model/package-info.java
@@ -0,0 +1,1 @@
+package com.hawkeye.model;
